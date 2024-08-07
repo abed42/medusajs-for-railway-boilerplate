28d7f57d19ee547c21635134685ebb3d90e18655
abed42 <abedhamami42@gmail.com>
2024-08-08 01:07:39 +0300

    changes product sorting text

--- medusajs-storefront/src/modules/store/components/refinement-list/sort-products/index.tsx
+++ medusajs-storefront/src/modules/store/components/refinement-list/sort-products/index.tsx
@@ -14,15 +14,7 @@ type SortProductsProps = {
 const sortOptions = [
   {
     value: "created_at",
-    label: "Latest Arrivals",
-  },
-  {
-    value: "price_asc",
-    label: "Price: Low -> High",
-  },
-  {
-    value: "price_desc",
-    label: "Price: High -> Low",
+    label: "Newest —>Oldest",
   },
 ]
 
